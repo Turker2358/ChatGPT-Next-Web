94fd7322bec8b57560781f80ed03dad15710c231
Turker2358 <139120506+Turker2358@users.noreply.github.com>
2024-05-14 13:18:25 +0800

    Update layout.tsx

--- app/layout.tsx
+++ app/layout.tsx
@@ -22,7 +22,7 @@ export const metadata: Metadata = {
     { media: "(prefers-color-scheme: dark)", color: "#151515" },
   ],
   appleWebApp: {
-    title: "NextChat",
+    title: "Turker's ChatGPT",
     statusBarStyle: "default",
   },
 };
